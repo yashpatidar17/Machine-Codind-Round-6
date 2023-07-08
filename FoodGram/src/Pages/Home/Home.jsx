42d3d56b8ed59c22c1835cd6f2a24f10b617c132
Yash Patidar <yashpatidar964@gmail.com>
2023-07-08 22:38:28 +0530

    Some fixes

--- FoodGram/src/Pages/Home/Home.jsx
+++ FoodGram/src/Pages/Home/Home.jsx
@@ -3,8 +3,8 @@ import { DataContext } from "../../Context/DataContextProvider";
 import "./home.css";
 import { Link } from "react-router-dom";
 export const Home = () => {
-  const { cusData,cuisineHandler,showData } = useContext(DataContext);
- 
+  const { cusData, cuisineHandler, showData } = useContext(DataContext);
+
   return (
     <div className="home">
       <h1>Food Ordering App </h1>
@@ -23,29 +23,30 @@ export const Home = () => {
       </div>
 
       <div>
+        {showData?.map((item) => (
+          <div key={item.id} className="menu">
+            <h2>Dishes by {item.name}</h2>
 
-          {showData?.map((item)=>(
-            <div key={item.id} className="menu">
-                <Link to={`/restaurant/${item.id}`}><h2>Dishes by {item.name}</h2></Link>
-                
-                <div className="menu-card">
-                {item.menu?.map((food)=>(
-                    <div className="menu-card-first">
-                        <img src={food.imgSrc} alt="food" className="food-img"/>
-                        <div className="menu-card-info">
-                        <span className="menu-p1">{food.name}</span>
-                        <span>{food.price} for {food.qty}</span>
-                        <span>{item.name}</span>
-                        </div>
-                    </div>
-                ))}
+            <div className="menu-card">
+              {item.menu?.map((food) => (
+                <div className="menu-card-first">
+                  <Link to={`/restaurant/${item.id}`}>
+                    <img src={food.imgSrc} alt="food" className="food-img" />
+                  </Link>
+
+                  <div className="menu-card-info">
+                    <span className="menu-p1">{food.name}</span>
+                    <span>
+                      {food.price} for {food.qty}
+                    </span>
+                    <span>{item.name}</span>
+                  </div>
                 </div>
+              ))}
             </div>
-          ))}  
-
+          </div>
+        ))}
       </div>
-
-
     </div>
   );
 };
